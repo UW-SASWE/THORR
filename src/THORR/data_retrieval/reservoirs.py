2125606df60efda505a463e4d57e2ef75db7f178
George Darkwah <61522557+gdarkwah@users.noreply.github.com>
2024-01-09 09:52:46 -0800

    Connect to database

--- src/THORR/data_retrieval/reservoirs.py
+++ src/THORR/data_retrieval/reservoirs.py
@@ -1,9 +1,20 @@
 # TODO: Add docstring
 
 import argparse
+import sys
+import geemap
+import ee
 from pathlib import Path
+import pandas as pd
+import geopandas as gpd
+import numpy as np
+import os
 from pathlib import Path
-from configparser import ConfigParser
+import time
+from random import randint
+import json
+import datetime
+
 
 # TODO: use the utils package to read the configuration file
 
@@ -44,8 +55,17 @@ def read_config(config_path, required_sections=[]):
 
     return config_dict
 
+# import connect
+# TODO: convert this to a function in the utils package
+def get_db_connection(package_dir, db_config_path):
+    utils = str(package_dir / "utils")
+    sys.path.insert(0, utils)
+    from sql import connect  # utility functions for connecting to MySQL
 
+    conn = connect.Connect(Path(db_config_path))
+    connection = conn.conn
 
+    return connection
 
 def main(args):
     config_path = Path(args.cfg)
@@ -56,6 +76,13 @@ def main(args):
     project_dir = Path(config_dict["project"]["project_dir"])
     db_config_path = project_dir / config_dict["mysql"]["db_config_path"]
 
+    # get database connection
+    connection = get_db_connection(
+        package_dir=Path(
+            config_dict["project"]["package_dir"]
+        ),  # base directory for the package
+        db_config_path=db_config_path,  # db_config_path
+    )
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
