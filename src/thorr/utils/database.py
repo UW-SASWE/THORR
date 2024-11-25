073b46a0129370a942632791051763d711b30b32
George Darkwah <61522557+gdarkwah@users.noreply.github.com>
2024-11-24 21:41:20 -0800

    new capability to connect with postgresql database

--- src/thorr/utils/database.py
+++ src/thorr/utils/database.py
@@ -1,49 +1,98 @@
-from mysql.connector import Error, connect
+import mysql.connector
+import psycopg
 import pandas as pd
 
 from thorr.utils import config as cfg
 
 
 class Connect:
-    def __init__(self, config_file, section="mysql", logger=None, return_conn=False):
+    def __init__(
+        self,
+        config_file,
+        section="mysql",
+        logger=None,
+        return_conn=False,
+        db_type="mysql",
+    ):
         self.config_file = config_file
         self.section = section
         self.logger = logger
+        self.db_type = db_type
+
+        if self.db_type == "mysql":
+            self.connect = mysql.connector.connect
+            self.Error = mysql.connector.Error
+        elif self.db_type == "postgresql":
+            self.connect = psycopg.connect
+            self.Error = psycopg.Error
+
         self.createConnection()
         self.return_conn = return_conn
         # self.createEngine()s
 
     def createConnection(self):
-        """Connect to MySQL database"""
+        """Connect to database"""
 
         db_config = cfg.read_config(self.config_file, [self.section])
         self.connection = None
 
         try:
             if self.logger is not None:
-                self.logger.info("Connecting to MySQL database...")
+                self.logger.info("Connecting to database...")
             else:
-                print("Connecting to MySQL database...")
-            self.connection = connect(
-                user=db_config[self.section]["user"],
-                database=db_config[self.section]["database"],
-                password=db_config[self.section]["password"],
-                host=db_config[self.section]["host"],
-                port=db_config[self.section]["port"],
-            )
+                print("Connecting to database...")
 
-            if self.connection.is_connected():
-                if self.logger is not None:
-                    self.logger.info("MySQL connection established.")
-                else:
-                    print("MySQL connection established.")
-            else:
-                if self.logger is not None:
-                    self.logger.info("MySQL connection failed.")
-                else:
-                    print("MySQL connection failed.")
+            if self.db_type == "mysql":
+                self.connection = self.connect(
+                    user=db_config[self.section]["user"],
+                    database=db_config[self.section]["database"],
+                    password=db_config[self.section]["password"],
+                    host=db_config[self.section]["host"],
+                    port=db_config[self.section]["port"],
+                )
 
-        except Error as error:
+                if self.connection.is_connected():
+                    if self.logger is not None:
+                        self.logger.info("Database connection established.")
+                    else:
+                        print("Database connection established.")
+                else:
+                    if self.logger is not None:
+                        self.logger.info("Database connection failed.")
+                    else:
+                        print("Database connection failed.")
+            elif self.db_type == "postgresql":
+                self.connection = self.connect(
+                    user=db_config[self.section]["user"],
+                    dbname=db_config[self.section]["dbname"],
+                    password=db_config[self.section]["password"],
+                    host=db_config[self.section]["host"],
+                    port=db_config[self.section]["port"],
+                )
+
+                if not self.connection.closed:
+                    if self.logger is not None:
+                        self.logger.info("Database connection established.")
+                    else:
+                        print("Database connection established.")
+                else:
+                    if self.logger is not None:
+                        self.logger.info("Database connection failed.")
+                    else:
+                        print("Database connection failed.")
+
+            # if self.connection.is_connected():
+            #     if self.logger is not None:
+            #         self.logger.info("Database connection established.")
+            #     else:
+            #         print("Database connection established.")
+            # else:
+            #     if self.logger is not None:
+            #         self.logger.info("Database connection failed.")
+            #     else:
+            #         print("Database connection failed.")
+
+        except self.Error as error:
             if self.logger is not None:
                 self.logger.error(error)
             else:
@@ -70,7 +119,7 @@ class Connect:
 
             return df
 
-        except Error as error:
+        except self.Error as error:
             if self.logger is not None:
                 self.logger.error(error)
             else:
@@ -84,7 +133,7 @@ class Connect:
                 self.logger.info("Connection closed.")
             else:
                 print("Connection closed.")
-        except Error as error:
+        except self.Error as error:
             if self.logger is not None:
                 self.logger.error(error)
             else:
