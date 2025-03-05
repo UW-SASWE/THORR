b0d44b341d33e9b33fd4124cce11f59823d355b8
George Darkwah <61522557+gdarkwah@users.noreply.github.com>
2025-03-05 12:12:08 -0800

    navigation depth: 2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,9 @@ templates_path = ["_templates"]
 # -- Options for HTML output
 
 html_theme = "sphinx_rtd_theme"
+html_theme_options = {
+    "navigation_depth": 2,
+}
 
 # -- Options for EPUB output
 epub_show_urls = "footnote"
