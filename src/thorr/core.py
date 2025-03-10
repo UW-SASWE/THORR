04c213c0c4156de562e22281fc5281cb7584a7de
George Darkwah <61522557+gdarkwah@users.noreply.github.com>
2025-03-10 10:18:43 -0700

    temperature estimate currently not available for reservoirs

--- src/thorr/core.py
+++ src/thorr/core.py
@@ -1,5 +1,9 @@
 from thorr.utils import read_config
 
 # temperature estimate
-def est_temp_reaches(config_path):
+def est_temp_reaches(config_path, element_type="reaches"):
     pass
+
+def est_temp_reservoirs(config_path, element_type):
+    print("Current version does not support temperature estimate for reservoirs")
+    print("Please use the reaches option")
