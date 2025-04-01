6f268322401a98d646143086b96ee079de48214c
George Darkwah <61522557+gdarkwah@users.noreply.github.com>
2025-04-01 11:21:33 -0700

    disable pretty exceptions and locals

--- src/thorr/cli.py
+++ src/thorr/cli.py
@@ -7,7 +7,7 @@ from thorr.data import retrieval
 
 from pathlib import Path
 
-app = typer.Typer(rich_markup_mode=None, pretty_exceptions_show_locals=False)
+app = typer.Typer(rich_markup_mode=None, pretty_exceptions_show_locals=False, pretty_exceptions_enable=False)
 
 
 @app.command()
